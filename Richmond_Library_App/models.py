edf6b0b2e674209ae8ee41476b229d217031423a
Lucas Patron <106820961+PatronLucas@users.noreply.github.com>
2023-09-28 22:12:58 -0500

    Updated models

--- Richmond_Library_App/models.py
+++ Richmond_Library_App/models.py
@@ -1,1 +1,21 @@
-import django.db.models
+import django.db.models as models
+
+
+# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    name = models.CharField(max_length=40)
+    email = models.EmailField(max_length=254)
+    user_type = models.CharField(max_length=10) # added user type
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=20)
+    year = models.CharField(max_length=4)
+    publisher = models.CharField(max_length=100)
+    copies = models.IntegerField()
+    available = models.IntegerField()
+    # added available
+
--- Richmond_Library_App/views/home.py
+++ Richmond_Library_App/views/home.py
@@ -4,3 +4,4 @@ from django.shortcuts import render
 class Home(View):
   def get(self, request):
     return render(request, "home.html", {})
+  
