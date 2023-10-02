aa63b1bc7a13db521408f35f5326e972d8405470
Lucas Patron <106820961+PatronLucas@users.noreply.github.com>
2023-10-01 20:52:49 -0500

    Logins functionalish

--- Richmond_Library_App/views/login.py
+++ Richmond_Library_App/views/login.py
@@ -1,7 +1,7 @@
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.forms import ModelForm
-
+from Richmond_Library_App.models import User
 
 
 
@@ -9,6 +9,20 @@ class Login(View):
   
     def get(self, request):
         return render(request, "login.html", {})
+    
+    def post(self, request):
+        name = request.POST.get('username')
+        password = request.POST.get('password')
+        if User().login(name, password):
+            request.session['name'] = name
+            request.session['password'] = password
+            return redirect('/home/')
+
+        else:
+            return render(request, "login.html", {"errorMessage": "Invalid username or password"})  
+
+
+
     
     # def post(self, request):
     #     name = request.POST.get('name')
