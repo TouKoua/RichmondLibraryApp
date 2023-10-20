7784cae0e93c616bd04d1b4a973555f675331731
Andrew Le <112992941+ale202008@users.noreply.github.com>
2023-10-20 00:12:18 -0500

    Some quick quality of life fixes.

--- Richmond_Library_App/views/login.py
+++ Richmond_Library_App/views/login.py
@@ -14,6 +14,15 @@ class Login(View):
         name = request.POST.get('username')
         password = request.POST.get('password')
         
+        # This is an if statement that uses the checkUser
+        # function to validify that the given username
+        # has an associated user in the database.
+        # for some reason it does not display message to
+        # login.html, will fix later.
+        if checkUser(name) == False:
+            context = {'errorMessage': 'User does not exist'}
+            return render(request, 'login.html', context)
+        
         # function that checks if the associated username
         # and password are of administrative status
         if Admin(name, password):
@@ -30,3 +39,11 @@ def Admin(name, password):
     user = User.objects.get(username=name, password=password)
     if user.user_type == 'admin':
         return True
+
+# function that checks if user is within the database
+def checkUser(name):
+    try:
+        user = User.objects.get(username=name)
+    except:
+        return False
+    return True
