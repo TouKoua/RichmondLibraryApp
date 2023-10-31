a7165688313671858d29974f75b8d1938c464ddd
nobodyname12 <83824462+nobodyname12@users.noreply.github.com>
2023-10-31 18:03:51 -0500

    Changed createuser to edituser

--- Richmond_Library_App/urls.py
+++ Richmond_Library_App/urls.py
@@ -20,7 +20,7 @@ from Richmond_Library_App.views.home import Home
 from Richmond_Library_App.views.login import Login, Logout
 from Richmond_Library_App.views.result import Result
 from Richmond_Library_App.views.book import BookPage
-from Richmond_Library_App.views.users import UsersPage, CreateUser
+from Richmond_Library_App.views.users import UsersPage, EditUser
 from Richmond_Library_App.views.profile import Profile 
 from Richmond_Library_App.views.addBook import BookCreateView
 
@@ -35,7 +35,7 @@ urlpatterns = [
     # the book name.
     path('book/<str:bookname>/', BookPage.as_view(), name="Book"),
     path('users/', UsersPage.as_view(), name="Users"),
-    path('users/createuser', CreateUser.as_view(), name="CreateUser"),
+    path('users/edituser', EditUser.as_view(), name="CreateUser"),
     path('add_book/', BookCreateView.as_view(), name='AddBook'),
     path('result/', Result.as_view(), name="Result"),
     path('profile/', Profile.as_view(), name="Profile"),
--- Richmond_Library_App/views/users.py
+++ Richmond_Library_App/views/users.py
@@ -8,17 +8,17 @@ class UsersPage(View):
         users = User.objects.all()
         return render(request, "users.html", {'users': users})
     
-class CreateUser(View):
+class EditUser(View):
     def get(self, request):
-        return render(request, "createuser.html")
+        return render(request, "edituser.html")
     
-    def post(self, request):
-        User.objects.create(
-            username=request.POST.get("username"),
-            password=request.POST.get("password"),
-            name=request.POST.get("name"),
-            email=request.POST.get("email"),
-            user_type=request.POST.get("user_type")
-        )
-        users = User.objects.all()
-        return render(request, "users.html", {'users': users})
+    # def post(self, request):
+    #     User.objects.create(
+    #         username=request.POST.get("username"),
+    #         password=request.POST.get("password"),
+    #         name=request.POST.get("name"),
+    #         email=request.POST.get("email"),
+    #         user_type=request.POST.get("user_type")
+    #     )
+    #     users = User.objects.all()
+    #     return render(request, "users.html", {'users': users})
