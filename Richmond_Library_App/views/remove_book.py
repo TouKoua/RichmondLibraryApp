7fee36e2ec483c30812b87918b015f637be9c1c3
TouKoua Xiong <70767264+TouKoua@users.noreply.github.com>
2023-11-09 21:33:29 -0600

    remove unneeded code

--- Richmond_Library_App/views/remove_book.py
+++ Richmond_Library_App/views/remove_book.py
@@ -10,11 +10,4 @@ def remove_book(request):
         book = Book.objects.get(id=book_id)
         book.delete()
 
-        # Delete the corresponding document from the Elasticsearch index
-        es = Elasticsearch([{'host': 'localhost', 'port': 9200}])  # Replace with your Elasticsearch server details
-        index_name = 'elastic'  # Replace with your Elasticsearch index name
-        document_id = str(book_id)
-
-        es.delete(index=index_name, id=document_id, ignore=[400, 404])
-
     return redirect('Home')  # Redirect to the home page or any other relevant page
