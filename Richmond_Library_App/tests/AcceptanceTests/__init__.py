f3fce3c6b53d40959fae5c143ade40328ea9545c
Andrew Le <112992941+ale202008@users.noreply.github.com>
2023-12-01 00:17:27 -0600

    last commit and changes.

--- Richmond_Library_App/tests/AcceptanceTests/__init__.py
+++ Richmond_Library_App/tests/AcceptanceTests/__init__.py
@@ -4,4 +4,13 @@ import os, django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Richmond_Library_App.settings")
 django.setup()
 
+from Richmond_Library_App.tests.AcceptanceTests.test_addBook import *
 from Richmond_Library_App.tests.AcceptanceTests.test_editUser import *
+from Richmond_Library_App.tests.AcceptanceTests.test_login import *
+from Richmond_Library_App.tests.AcceptanceTests.test_logout import *
+from Richmond_Library_App.tests.AcceptanceTests.test_removeBook import *
+
+# To run all tests within the direct use the command below:
+# python manage.py test Richmond_Library_App.tests.AcceptanceTests
+# To run a specific test file within the direct use the command:
+# python manage.py test Richmond_Library_App.tests.AcceptanceTests.(test file name)
--- Richmond_Library_App/tests/UnitTests/test_login.py
+++ Richmond_Library_App/tests/UnitTests/test_login.py
@@ -1,27 +1,17 @@
-import sys
-sys.path.append(r"C:\Users\Proje\Desktop\Projects\RichmondLibraryApp")
-import os, django
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Richmond_Library_App.settings")
-django.setup()
-
 from django.test import TestCase, Client
-from django.urls import reverse
 from Richmond_Library_App.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth.hashers import make_password
 
-class LoginTestCase(TestCase): 
+class LoginTests(TestCase):
     def setUp(self):
         self.client = Client()
-        self.login_url = reverse('Login')  # Using correct name 'Login'
-        self.home_url = reverse('Home')  # Using correct name 'Home'
-        # Create a test user for login
-        self.user = User.objects.create(username='testuser', password='password123', email='testuser@example.com')
-
-    def test_login_user(self):
-        # Attempt to log in with valid credentials
-        login_data = {'username': 'testuser', 'password': 'password123', 'email' : 'testuser@example.com'}
-        response = self.client.post(self.login_url, login_data, follow=True)
+        password = 'password123'
+        self.password = make_password(password)
+        User.objects.create(username="test", password=self.password, email="test@email.com", user_type="student")
+    
+    def test_user_login(self):
+        self.user = authenticate(username='test', password='password123') 
+        login = self.client.login(username='test', password='password123') 
+        self.assertTrue(login)
         
-        # Check if the user is redirected to the home page upon successful login
-        self.assertRedirects(response, self.home_url)
-        # Check if the user is logged in
-        self.assertTrue('_auth_user_id' in self.client.session)
